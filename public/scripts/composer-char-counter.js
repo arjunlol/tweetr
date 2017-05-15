b3e108a407accd3e9c8331b86576f8fcd1c7d1ef
Arjun Lall <lallarjun@gmail.com>
2017-05-15 21:51:27 +0000

    added js to count textarea input and update counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,13 @@
+$(document).ready( function() {
+  $('.new-tweet textarea').on('input', function() {
+    let counter = $(this).closest('.new-tweet').find('.counter');
+    if(($(this).val().length > 140)) {
+      counter.addClass('over140');
+    } else {
+      counter.removeClass('over140');
+    }
+    counter.text(140 - $(this).val().length);
+  });
+
+
+});
