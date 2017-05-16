fd10776bd029323dbb99195ee006d1fecce204e0
Arjun Lall <lallarjun@gmail.com>
2017-05-16 17:40:44 +0000

    added createTweetElement function  that takes in a tweet object and is responsible for returning a tweet <article> element containing the entire HTML structure of the tweet.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,9 +3,82 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+ $(document).ready( function() {
+  // Fake data taken from tweets.json
+  var data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": {
+          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+        },
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": {
+          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+        },
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    },
+    {
+      "user": {
+        "name": "Johann von Goethe",
+        "avatars": {
+          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+        },
+        "handle": "@johann49"
+      },
+      "content": {
+        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+      },
+      "created_at": 1461113796368
+    }
+  ];
 
-$(document).ready( function() {
-  createTweetElement function () {
+  function renderTweets(tweets) {
+    // loops through tweets
+      // calls createTweetElement for each tweet
+      // takes return value and appends it to the tweets container
+    $('#tweets').append()
+  }
 
-  };
+  function createTweetElement(tweetobj) {
+    let html = `<header>
+      <img src=${tweetobj.user.avatars.small}/>
+      <h2 class="tweet-name">${tweetobj.user.name}</h2>
+      <p class="tweet-tag">${tweetobj.user.handle}</p>
+    </header>
+    <p class="tweet-text">${tweetobj.content.text}</p>
+    <footer>
+      <p class="date">${tweetobj.created_at}</p>
+      <span>
+      <img src='https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-ios7-flag-128.png'/>
+      <img src='https://cdn0.iconfinder.com/data/icons/entypo/100/retweet-128.png'/>
+      <img src='https://cdn2.iconfinder.com/data/icons/pittogrammi/142/80-128.png'/>
+      </span>
+    </footer>`
+
+    let $tweet = $('<article>').append(html);
+    return $tweet;
+  }
+
+renderTweets(data);
+
 });
